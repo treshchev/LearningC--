b2475fe848131b895e6ca2502840aeb8da870905
treshchev <letsgovlad@gmail.com>
2024-01-02 15:52:29 +0300

    New Game

--- Games from book/Games from book.cpp
+++ Games from book/Games from book.cpp
@@ -1,16 +1,44 @@
-//Программа Game Stats 3.0
-//Демонстрирует работу с константами
+// Lost treasure
+// Adventure Game Personalization
 #include <iostream>
-int main()
-{
-	const int ALIEN_POINTS = 150;
-	int aliensKilled = 10;
-	int score = aliensKilled * ALIEN_POINTS;
-	std::cout << "score: " << score << std::endl;
-	enum difficulty { NOVICE, EASY, NORMAL, HARD, UNBEATABLE };
-	difficulty myDifficulty = EASY;
-	enum shipCost { FIGHTER_COST = 26, BOMBER_COST, CRUISER_COST = 100 };
-	shipCost myShipCost = BOMBER_COST;
-	std::cout << "\nTo upgrade my ship to a Cruiser will cost " << (CRUISER_COST - myShipCost) << " Resourse Points. \n " << std::endl;
-	return 0;
+#include <string>
+
+
+
+int main() {
+
+const int GOLD_PIECES = 900;
+int adventures, killed, survivors;
+std::string leader;
+    // Getting information
+    std::cout << "Welcome to Lost Fortune\n\n";
+    std::cout << "Please enter the following for your personalized adventure\n";
+
+    std::cout << "Enter a number: ";
+    std::cin >> adventures;
+
+    std::cout << "Enter a number smaller than the first: ";
+    std::cin >> killed;
+    survivors = adventures - killed;
+
+    std::cout << "Enter your last name: ";
+    std::cin >> leader;
+
+    // Plot
+    std::cout << "\nA brave group of " << adventures << " set out on a quest " << std::endl;
+    std::cout << "- - in search of the lost reasure of the Ancient Dwarves. " << std::endl;
+    std::cout << "The group was led by that legendary rogue, " << leader << ".\n" << std::endl;
+    std::cout << "\nAlong the way, a band of maruarding ogres abbushed the party. " << std::endl;
+    std::cout << "All fought braveky under the command of " << leader << std::endl;
+    std::cout << "and the ogres were defeated, but at a cost. " << std::endl;
+    std::cout << "Of the adventures, " << killed << " were vanquished" << std::endl;
+    std::cout << "leaving just" << survivors << " in the group.\n" << std::endl;
+    std::cout << "\nThe party was about to give up all hope. " << std::endl;
+    std::cout << "But while laying the deceased to rest, " << std::endl;
+    std::cout << "they stumbled upon the buried fortune. " << std::endl;
+    std::cout << "So the adventures split " << GOLD_PIECES << " gold pieces. ";
+    std::cout << leader << " held on to the extra " << (GOLD_PIECES % survivors);
+    std::cout << " pieces to keep things fair of course.\n";
+
+    return 0;
 }
