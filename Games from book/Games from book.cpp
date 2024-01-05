bf8b36e3246861e0239a3bcd74be243974d366eb
treshchev <letsgovlad@gmail.com>
2024-01-05 16:38:25 +0300

    Some work with if

--- Games from book/Games from book.cpp
+++ Games from book/Games from book.cpp
@@ -1,21 +1,31 @@
+//Программа Score Rater 
+// Прогрмма демонстриурет работу с инструкцией if
 #include <iostream>
 int main() {
-
-	long double score1, score2, score3;
-	
-	std::cout << "Enter your points amount for the first time: ";
-	std::cin >> score1;
-
-	std::cout << "Enter your point total a second time: ";
-	std::cin >> score2;
-
-	std::cout << "Enter your points amount a third time: ";
-	std::cin >> score3;
-
-	long double AverageScore;
-	AverageScore = (score1 + score2 + score3) / 3.0;
-	std::cout << "Average value based on three: " << AverageScore;
-
-
+	if (true)
+	{
+		std::cout << "This is always displayed.\n\n";
+	}
+	if (false)
+	{
+		std::cout << "This is never displayed.\n\n";
+	}
+	int score = 1000;
+	if (score)
+	{
+		std::cout << "At least you didnt score zero.\n\n";
+	}
+	if (score >= 350)
+	{
+		std::cout << "You scored 350 or more. Decent.\n\n";
+	}
+	if (score >= 500)
+	{
+		std::cout << "You scored 500 or more. Nice.\n\n";
+		if (score >= 1000)
+		{
+			std::cout << "You scored 1000 or more. Impressive!\n";
+		}
+	}
 	return 0;
 }
