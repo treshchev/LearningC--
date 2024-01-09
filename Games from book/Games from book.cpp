5eff1ba09ad51f68ee2683d2c0a63a494ea280fe
treshchev <letsgovlad@gmail.com>
2024-01-09 18:11:59 +0300

    New Game

--- Games from book/Games from book.cpp
+++ Games from book/Games from book.cpp
@@ -1,18 +1,26 @@
-// Программа Score Rater 2.0
-// Демонстрирует работу с условием else
+// Программа Score Rater 3.0
+// Демонстрирует работу с последовательностью if else - if else
 #include <iostream>
-int main() {
+int main() 
+{
 	int score;
 	std::cout << "Enter your score: ";
 	std::cin >> score;
 	if (score >= 1000)
 	{
-		std::cout << "You scored 1000 or more. Impressive!\n";
+		std::cout << "You scored 1000 or more";
+	}
+	else if (score >= 500)
+	{
+		std::cout << "You scored 500 or more. Nice.\n";
+	}
+	else if (score >= 250)
+	{
+		std::cout << "You scored 250 or more. Decent.\n";
 	}
 	else
 	{
-		std::cout << "You scored less than 1000.\n";
+		std::cout << "You scored less than 250. Nothing to brag about.\n";
 	}
 	return 0;
-
 }
