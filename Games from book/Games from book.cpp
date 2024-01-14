6a797d1d53125ed79b6886823d8452c3435a0d75
treshchev <letsgovlad@gmail.com>
2024-01-14 21:58:29 +0300

    some changes

--- Games from book/Games from book.cpp
+++ Games from book/Games from book.cpp
@@ -1,36 +1,16 @@
-// Программа Menu Chooser
-// Демонстрирцет работу с интсрукццией switch
+// Программа Play Again
+// Демонстрирует работу с циклом while
 #include <iostream>
 int main()
 {
-	std::cout << "Difficulty Levels\n\n";
-	std::cout << "1 - Easy\n";
-	std::cout << "2 - Normal\n";
-	std::cout << "3 - Hard\n";
-	int choice;
-	std::cout << "Choice: ";
-	std::cin >> choice;
-
-	switch (choice)
+	char again = 'y';
+	while (again == 'y')
 	{
-		// в команде case можно написать любое число, 
-		// и в зависимости от написанного числа, будет выводится тот или иной текст
-		// I write these commands for myself to better understand the code and various commands
-	case 1:
-		std::cout << "You picked Easy.\n";
-		break;
-	case 2:
-		std::cout << "You picked Normal\n";
-		break;
-	case 3:
-		std::cout << "You picked Hard\n";
-		break;
-		// команда default используется, если мы введём любое другое число (кроме 1, 2, 3) то у нас
-		// на экран выводится именно этот текст, который мы написали в default 
-		// I write these commands for myself to better understand the code and various commands
-	default:
-		std::cout << "You made an illegal choice.\n";
+		std::cout << "\n**Played an exiting game**";
+		std::cout << "\nDo you want to play again? (y/n): ";
+		std::cin >> again;
 
 	}
+	std::cout << "\nOkay. bye.";
 	return 0;
 }
