70c4aa952cabdf33931a7f832348d4ada2a66b76
treshchev <letsgovlad@gmail.com>
2024-01-30 21:17:40 +0300

    Login and Password

--- Games from book/Games from book.cpp
+++ Games from book/Games from book.cpp
@@ -1,25 +1,52 @@
-// Программа Finicky Counter
-// Демонстрирует работу с интрукциями break и  continue
+// Программа Designer Network
+// Демонстрирует работу с логическими операторами
 #include <iostream>
+#include <string>
 int main()
 {
-	int count = 0;
-	while (true)  // задаю значение true и теперь цикл не может быть false
-	{
-		count++;
-		// заканчиваес цикл, если значение count превышает 10
+    std::string username;
+    std::string password;
 
-		if (count > 10) // тут я задаю параментры бесконечного цикла, так как true это бесконечный цикл
-		{
-			break;
-		}
+    bool succes;
+    std::cout << "\nGame Designer's Network\n";
+        do
+        {
+            std::cout << "\nUsername: ";
+            std::cin >> username;
+            std::cout << "Password: ";
+            std::cin >> password;
 
-		// пропускаем число 5
-		if (count == 5)
-		{
-			continue;
-		}
-		std::cout << count << std::endl;
-	}
-	return 0;
+            if (username == "qwe" && password == "qwe123")
+            {
+                std::cout << "\nHello Sir.";
+                succes = true;
+            }
+
+            else if (username == "qaz" && password == "qaz123")
+            {
+                std::cout << "\nWhat's up bro!";
+                succes = true;
+            }
+
+            else if (username == "wsx" && password == "wsx123")
+            {
+                std::cout << "\nHow is it going?";
+                succes = true;
+            }
+
+            else if (username == "guest" || password == "guest")
+            {
+                std::cout << "\nWelcome guest";
+                succes = true;
+            }
+
+            else
+            {
+                std::cout << "\nYour login or password failed.";
+                succes = false;
+            }
+
+        } while (!succes);
+
+          return 0;
 }
