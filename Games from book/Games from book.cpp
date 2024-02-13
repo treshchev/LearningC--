643f6e9f0bf876ee2ca38885e6e0d4c2f3d162d5
treshchev <letsgovlad@gmail.com>
2024-02-13 20:49:03 +0300

    some changes

--- Games from book/Games from book.cpp
+++ Games from book/Games from book.cpp
@@ -8,10 +8,10 @@ int main()
 {
 	srand(static_cast <unsigned int> (time(0)));
 	// запускаем генератор случайных чисел
-	int  randomNumber = rand(); // генерируем случайное число
-	int die = (randomNumber % 6) + 1; // получаем число можду 1 и 6
+	int  randomNumber = rand(); // генерируем случайное число, задаю значение от 0 до 32 767
+	int die = (randomNumber % 6) + 1; // получаем число можду 1 и 6, тут просто если любое положительное число подалить на 6 и прибавить + 1 то будет диапазон от 0 до 6
 		std::cout << "You rolled a " << die << std::endl;
 
-		std::cout << "A number: " << RAND_MAX << std::endl;  // это просто проверка, диапазона чисел, где мы генерируем случайное число
+		std::cout << "A max number: " << RAND_MAX << std::endl;  // это просто проверка, диапазона чисел, где мы генерируем случайное число
 	return 0;
 }
